2e32f95192e29c0de7c8a0bc113a41ae464e6cf8
14link <40344950+14link@users.noreply.github.com>
2024-07-12 21:31:13 +0800

    更新 customize.sh

--- customize.sh
+++ customize.sh
@@ -20,3 +20,8 @@ sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-kS'/g" openwrt/package/base-files
 #调整&&去重,删除不使用的源文件
 rm -rf openwrt/package/lean/autosamba
 rm -rf openwrt/feeds/luci/applications/luci-app-netdata
+#4. git clone
+        # git clone https://github.com/sirpdboy/autosamba package/autosamba
+        # git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+        # git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
+        # sed -i 's/PATCHVER:=6.6/PATCHVER:=6.1/g' target/linux/x86/Makefile
