585018b06850217c24418199a686aced2013bad5
14link <40344950+14link@users.noreply.github.com>
2025-09-08 19:40:33 +0800

    1

--- imm_customize.sh
+++ imm_customize.sh
@@ -13,8 +13,8 @@ sed -i 's/192.168.1.1/192.168.0.252/g' openwrt/package/base-files/files/bin/conf
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/default-settings/files/zzz-default-settings
 #
 # sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kSir '/g" openwrt/package/default-settings/files/zzz-default-settings
+# sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kSir '/g" openwrt/package/emortal/default-settings/files/99-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='OpWrt-imm'/g" openwrt/package/base-files/files/bin/config_generate
-#
 #3. Replace with JerryKuKu’s Argon
 	#	 rm openwrt/package/lean/luci-theme-argon -rf
 #调整&&去重,删除不使用的源文件
@@ -25,3 +25,8 @@ sed -i "s/hostname='OpenWrt'/hostname='OpWrt-imm'/g" openwrt/package/base-files/
         # git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
         # git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
         # sed -i 's/PATCHVER:=6.6/PATCHVER:=6.1/g' target/linux/x86/Makefile
+
+# cp -f $GITHUB_WORKSPACE/99-default-settings package/emortal/default-settings/files/99-default-settings
+# cp -f $GITHUB_WORKSPACE/banner package/base-files/files/etc/banner
+
+# echo "uci set luci.main.mediaurlbase=/luci-static/argon" >> $ZZZ
