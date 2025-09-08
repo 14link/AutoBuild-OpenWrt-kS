3e0603314a7f0c029efeb58c833b36413637ec1a
14link <40344950+14link@users.noreply.github.com>
2025-09-08 15:05:45 +0800

    更新 Imm-customize.sh

--- Imm-customize.sh
+++ Imm-customize.sh
@@ -7,13 +7,13 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.0.251/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.252/g' openwrt/package/base-files/files/bin/config_generate
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 # sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/default-settings/files/zzz-default-settings
 #
 sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kSir '/g" openwrt/package/default-settings/files/zzz-default-settings
-sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-imm'/g" openwrt/package/base-files/files/bin/config_generate
+sed -i "s/hostname='OpenWrt'/hostname='OpWrt-imm'/g" openwrt/package/base-files/files/bin/config_generate
 #
 #3. Replace with JerryKuKu’s Argon
 	#	 rm openwrt/package/lean/luci-theme-argon -rf
